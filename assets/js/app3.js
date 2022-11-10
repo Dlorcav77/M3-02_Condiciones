f47837b774aa32ff9268c170493afbe96ed2df09
Diego_Lorca <diego.lorcaveliz@gmail.com>
2022-11-10 17:42:02 -0300

    03-condidicones

--- assets/js/app3.js
+++ assets/js/app3.js
@@ -1,11 +1,25 @@
-const btnImg      = document.querySelector("#img");
-const card      = document.querySelector("#card");
+const cod1      = document.querySelector("#cod1");
+const cod2      = document.querySelector("#cod2");
+const cod3      = document.querySelector("#cod3");
+const ingresar  = document.querySelector("#ingresar");
+const mensaje   = document.querySelector("#mensaje");
 
-btnImg.addEventListener("click", () => {
+ingresar.addEventListener("click", () => {
 
-  console.log('entro')
-/*      
-  card.style.backgroundColor='yellow';
-  card.style.border='30px';
-*/
+  const codigo1 = +cod1.value;
+  const codigo2 = +cod2.value;
+  const codigo3 = +cod3.value;
+
+  if(codigo1===9 && codigo2===9 && codigo3===1)
+  {
+    mensaje.textContent= 'PassWord 1 correcto' ;
+    return;
+  }
+  if(codigo1===7 && codigo2===1 && codigo3===4)
+  {
+    mensaje.textContent= 'PassWord 2 correcto' ;
+    return;
+  }
+
+  mensaje.textContent= 'PassWord incorrecto' ;
 });
